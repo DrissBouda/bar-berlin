0a9363f0778c402ccb00d6101bc51b3a17ad0d66
Driss El Youby <driss@Drisss-Laptop.local>
2023-11-23 13:29:57 +0000

    first commit

---
+++ app/speisekarte/page.tsx
@@ -0,0 +1,27 @@
+export default function Hola() {
+  return (
+    <div className="mockup-window border border-base-300 m-5">
+      <div className="px-4 py-16 border-t border-base-300">
+        <div>
+          <h3>Auszug aus unserer Speisekarte</h3>
+        </div>
+        <ul>
+          <li>Bunte Blattsalate</li>
+          <li>Currywurst mit scharfen Zwiebeln und Pommes</li>
+          <li>Bratwurst mit lecker Kartoffelsalat</li>
+          <li>Nürnberger Rostbratwurst mit Sauerkraut und Bratkartoffeln</li>
+          <li>Gemüsepfanne mit Kartoffeln und Käse überbacken</li>
+          <li>Schweinskopfsülze mit Remouladensoße und Bratkartoffeln</li>
+          <li>Schnitzel Wiener Art mit Pommes und Salat</li>
+          <li>Paprikaschnitzel, pikant</li>
+          <li>Jägerschnitzel</li>
+          <li>Schnitzel Bar Berlin</li>
+          <li>Rindernuggets mit Gemüse</li>
+          <li>Rumpsteak mit Zwiebeln, Pommes und Salat</li>
+          <li>diverse Desserts</li>
+          <li>ab Oktober Sonntags Kuchen</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
