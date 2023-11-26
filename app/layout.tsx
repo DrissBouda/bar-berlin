2dd37faccc0cde86dc5cf817bb77e3f603b221e8
Driss El Youby <driss@Drisss-Laptop.local>
2023-11-26 13:20:49 +0000

    commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,9 @@ export default function RootLayout({
 }) {
   return (
     <html data-theme="light" lang="en">
+      <head>
+        <link rel="shortcut icon" href="/favicon.png" sizes="any" />
+      </head>
       <body>
         <div className="md:mx-40 sm:mx-0 xs:mx-40">
           <div className="navbar bg-base-100">
@@ -57,6 +60,23 @@ export default function RootLayout({
                   </label>
                 </div>
                 <div className="drawer-side z-[1]">
+                  <div className="z-[2]">
+                    <label
+                      className="btn btn-circle drawer-overlay"
+                      htmlFor="my-drawer-4"
+                    >
+                      {/* close icon */}
+                      <svg
+                        className="swap-off fill-current drawer-overlay"
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="32"
+                        height="32"
+                        viewBox="0 0 512 512"
+                      >
+                        <polygon points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49" />
+                      </svg>
+                    </label>
+                  </div>
                   <label
                     htmlFor="my-drawer-4"
                     aria-label="close sidebar"
