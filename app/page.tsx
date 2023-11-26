ee7a6c30316098d30981d1b748dd330061aaf741
Driss El Youby <driss@Drisss-Laptop.local>
2023-11-26 15:48:45 +0000

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -13,29 +13,34 @@ export default function Home() {
         <div className="hero-overlay bg-opacity-50"></div>
         <div className="hero-content text-center text-neutral-content">
           <div className="max-w-md">
-            <h1 className="mb-5 text-5xl font-bold text-warning shadow-md bar-berlin">
-              Bar Berlin Teneriffa Meets Sylt
+            <h1 className="mb-4 text-5xl font-bold text-warning shadow-md bar-berlin">
+              Bar Berlin Teneriffa
             </h1>
             <p className="mb-5 bar-berlin">
               Playa Salytien, Lokal Nr. 17 38660 Playa de Las Americas, Adeje
               Santa Cruz de Tenerife +34 922 787 188
             </p>
-            <Link href="/speisekarte">
-              <button className="btn btn-warning m-3">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                  className="w-6 h-6"
-                >
-                  <path d="M7.5 3.375c0-1.036.84-1.875 1.875-1.875h.375a3.75 3.75 0 013.75 3.75v1.875C13.5 8.161 14.34 9 15.375 9h1.875A3.75 3.75 0 0121 12.75v3.375C21 17.16 20.16 18 19.125 18h-9.75A1.875 1.875 0 017.5 16.125V3.375z" />
-                  <path d="M15 5.25a5.23 5.23 0 00-1.279-3.434 9.768 9.768 0 016.963 6.963A5.23 5.23 0 0017.25 7.5h-1.875A.375.375 0 0115 7.125V5.25zM4.875 6H6v10.125A3.375 3.375 0 009.375 19.5H16.5v1.125c0 1.035-.84 1.875-1.875 1.875h-9.75A1.875 1.875 0 013 20.625V7.875C3 6.839 3.84 6 4.875 6z" />
-                </svg>
-                Speisekarte
-              </button>
-            </Link>
+            <div className="indicator m-8">
+              <span className="indicator-item badge badge-error text-white bar-berlin">
+                +10
+              </span>
+              <Link href="/speisekarte">
+                <button className="btn btn-warning">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path d="M7.5 3.375c0-1.036.84-1.875 1.875-1.875h.375a3.75 3.75 0 013.75 3.75v1.875C13.5 8.161 14.34 9 15.375 9h1.875A3.75 3.75 0 0121 12.75v3.375C21 17.16 20.16 18 19.125 18h-9.75A1.875 1.875 0 017.5 16.125V3.375z" />
+                    <path d="M15 5.25a5.23 5.23 0 00-1.279-3.434 9.768 9.768 0 016.963 6.963A5.23 5.23 0 0017.25 7.5h-1.875A.375.375 0 0115 7.125V5.25zM4.875 6H6v10.125A3.375 3.375 0 009.375 19.5H16.5v1.125c0 1.035-.84 1.875-1.875 1.875h-9.75A1.875 1.875 0 013 20.625V7.875C3 6.839 3.84 6 4.875 6z" />
+                  </svg>
+                  Speisekarte
+                </button>
+              </Link>
+            </div>
             <div className="indicator">
-              <span className="indicator-item badge badge-error text-white">
+              <span className="indicator-item badge badge-error text-white bar-berlin">
                 Neu
               </span>
               <Link href="/programm">
